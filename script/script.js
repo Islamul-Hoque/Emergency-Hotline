9708b730dd0c3475893082c0d0a9fd8b345f57e0
Islamul-Hoque <islamulhoque2006@gmail.com>
2025-08-27 17:40:16 +0600

    Integrate call buttons functionality: alert, coin deduction, and call history logging

---
+++ script/script.js
@@ -0,0 +1,53 @@
+
+
+// Heart Functionalities
+getId('Left-Side-container').addEventListener('click', function(e){
+    if(e.target.className.includes('fa-heart')){
+        const loveBtn =e.target;
+
+        const initialHeart = Number(getId('heart').innerText);
+        const currentHeart = initialHeart + 1;
+        getId('heart').innerText = currentHeart;
+
+    }
+})
+
+
+//Call Buttons Functionalities
+getId('Left-Side-container').addEventListener('click', function(e){
+    if(e.target.closest('.call-btn')){
+        const callBtn = e.target.closest('.call-btn');
+
+        const serviceName = callBtn.parentNode.parentNode.children[1].children[0].innerText;
+        const callNumber = callBtn.parentNode.parentNode.children[2].children[0].innerText;
+        const time = new Date().toLocaleTimeString();
+
+    // Coins Calculation
+        const initialCoin = Number(getId('coins').innerText);
+
+        if(initialCoin < 20){
+            return alert('❌ You don’t have enough coins! You need at least 20 coins to make a call.')
+        }
+
+        const currentCoins = initialCoin - 20;
+        getId('coins').innerText = currentCoins;
+        alert(`📞 Calling ${serviceName} ${callNumber} ...`);
+
+    // Get Call History Container
+            const callHistoryContainer = getId('Call-History-Container');
+            const createDiv = document.createElement('div')
+            createDiv.innerHTML = `
+                <div class="bg-[#f2f2f2] px-3 py-2 mb-2 rounded-[0.8rem] flex justify-between items-center">
+                    <div>
+                        <h2 class="text-[0.7rem] font-semibold">${serviceName}</h2>
+                        <p class="text-sm text-[#5c5c5c]">${callNumber}</p>
+                    </div>
+                    <div class="text-[0.7rem]">${time}</div>
+                </div>`;
+            callHistoryContainer.appendChild(createDiv);
+    }
+})
+
+
+
+
